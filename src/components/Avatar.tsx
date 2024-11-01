f4ff20b1333e6db1267d99e8459af786b1b841ac
Igor Pereira Campos <igorpcampos2004@gmail.com>
2024-11-01 08:47:39 -0300

    fix: resolvendo merge

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,33 +1,61 @@
 import Image from "next/image";
 import React from "react";
 
-export default function Avatar({ head, body, arm }) {
+export default function Avatar({ head, body, legs, arms }) {
   return (
-    <div className="relative w-36 h-44">
+    <div className="relative w-36 h-80 flex flex-col items-center">
       {/* Cabeça */}
-      <Image
-        src={head}
-        alt="Head"
-        width={100}  // Ajuste o valor conforme necessário
-        height={100} // Ajuste o valor conforme necessário
-        className="absolute top-0 left-1/2 transform -translate-x-1/2 z-30"
-      />
+      {head && (
+        <Image
+          src={head}
+          alt="Head"
+          width={80}
+          height={80}
+          className="z-30"
+        />
+      )}
+
       {/* Corpo */}
-      <Image
-        src={body}
-        alt="Body"
-        width={900}  // Ajuste o valor conforme necessário
-        height={300} // Ajuste o valor conforme necessário
-        className="absolute top-24 left-1/2 transform -translate-x-1/2 z-20"
-      />
-      {/* Braço ou Arma */}
-      <Image
-        src={arm}
-        alt="Arm or Weapon"
-        width={100}  // Ajuste o valor conforme necessário
-        height={100} // Ajuste o valor conforme necessário
-        className="absolute top-20 left-1/4 transform -translate-x-1/2 z-10"
-      />
+      {body && (
+        <Image
+          src={body}
+          alt="Body"
+          width={80}
+          height={100}
+          className="z-20"
+        />
+      )}
+
+      {/* Braços */}
+      {arms && (
+        <>
+          <Image
+            src={arms}
+            alt="Arms"
+            width={30}
+            height={50}
+            className="absolute top-16 left-0 z-10" // Alterado para top-8
+          />
+          <Image
+            src={arms}
+            alt="Arms"
+            width={30}
+            height={50}
+            className="absolute top-16 right-0 z-10" // Alterado para top-8
+          />
+        </>
+      )}
+
+      {/* Pernas */}
+      {legs && (
+        <Image
+          src={legs}
+          alt="Legs"
+          width={80}
+          height={80}
+          className="z-10"
+        />
+      )}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,16 @@ import Avatar from "../components/Avatar";
 import AvatarCustomizer from "../components/AvatarCustomizer";
 
 export default function HomePage() {
-  const [head, setHead] = useState("/images/head.png");
-  const [body, setBody] = useState("/images/body.png");
-  const [arm, setArm] = useState("/images/arms.png");
+  const [head, setHead] = useState("/images/ninja_head_green.png");
+  const [body, setBody] = useState("/images/ninja_body_green.png");
+  const [arms, setArms] = useState("/images/ninja_arm_green.png");
+  const [legs, setLegs] = useState("/images/ninja_legs_green.png");
 
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-6">Avatar Customizer</h1>
-      <Avatar head={head} body={body} arm={arm} />
-      <AvatarCustomizer setHead={setHead} setBody={setBody} setArm={setArm} />
+      <Avatar head={head} body={body} arms={arms} legs={legs} />
+      <AvatarCustomizer setHead={setHead} setBody={setBody} setArms={setArms} setLegs={setLegs} />
     </div>
   );
 }
